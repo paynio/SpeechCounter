85e101ef9968362bf44c609c2e22e4ba6f122a84
Dan Payne <dan@cyanfive.com>
2013-04-12 12:06:59 +0100

    Initial commit

--- SpeechCounter/main.m
+++ SpeechCounter/main.m
@@ -7,15 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        static NSInteger count = 0;
+        NSArray *voices = [NSSpeechSynthesizer availableVoices];
+        NSSpeechSynthesizer *syn = [[NSSpeechSynthesizer alloc]initWithVoice:[voices objectAtIndex:2]];
+        [syn setRate:300.0];
         
+        CFRunLoopTimerRef timer = CFRunLoopTimerCreateWithHandler(NULL, CFAbsoluteTimeGetCurrent(), 1.0, 0, 0, ^(CFRunLoopTimerRef timer){
+            NSLog(@"count = %ld", (long)count);
+            
+            NSString *countString = [NSString stringWithFormat:@"%ld", (long)count];
+            [syn startSpeakingString:countString];
+            count++;
+            if (count == 121)
+                count = 1;
+        });
+        
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes);
+        CFRunLoopRun();
     }
     return 0;
 }
