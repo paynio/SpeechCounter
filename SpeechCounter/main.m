926f58a9e71f85f1b0d45036a41f81d65986b235
Dan Payne <dan@cyanfive.com>
2013-04-12 11:33:33 +0100

    Initial Commit

---
+++ SpeechCounter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SpeechCounter
+//
+//  Created by Daniel Payne on 12/04/2013.
+//  Copyright (c) 2013 o2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
